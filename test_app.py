64d4a7d755dff30091ace95ada2013d5e66e9b23
Tabasum khan <tabasumkhanmca@gmail.com>
2024-07-11 12:07:01 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,17 +1,34 @@
+# import pytest
+# from app import create_app
+# from flask import Flask
+# from urllib.parse import quote
+
+# @pytest.fixture
+# def app():
+#     return create_app()
+
+# @pytest.fixture
+# def client(app):
+#     return app.test_client()
+
+# def test_home(client):
+#     response = client.get('/')
+#     assert response.status_code == 200
+#     expected_text = 'Well done! You Successfully deployed your flask app, Now you can start building your app.'
+#     assert expected_text.encode() in response.data
+
 import pytest
-from app import create_app
-from urllib.parse import quote
+from flask import Flask
+from app import create_app  # Replace `your_module` with the actual module name where `create_app` is defined
 
 @pytest.fixture
-def app():
-    return create_app()
-
-@pytest.fixture
-def client(app):
-    return app.test_client()
+def client():
+    app = create_app()
+    with app.test_client() as client:
+        yield client
 
 def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    expected_text = 'Well done! You Successfully deployed your flask app, Now you can start building your app.'
+    expected_text = 'Well done! You Successfully added and deployed your flask app, Now you can start building your app.'
     assert expected_text.encode() in response.data
